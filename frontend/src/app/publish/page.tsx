ab6637555db63752b0b2833e54013559e66248c4
mxber2022 <mxber2022@gmail.com>
2023-12-09 03:59:31 +0530

    publish

---
+++ frontend/src/app/publish/page.tsx
@@ -0,0 +1,67 @@
+"use client"
+import { useAccount, useConnect, useEnsName } from 'wagmi'
+import  WakuNodeCAPI  from '@/context/WakuNodeRunContextApi';
+import { useContext, useEffect } from 'react';
+import { useState } from 'react';
+
+interface Data {
+    Title: string;
+    Image: string;
+    Content: string;
+}
+
+function publish () {
+    const { address, isConnected } = useAccount();
+
+    const [formData, setFormData] = useState<Data>({
+        Title: '',
+        Image: '',
+        Content: '',
+    });
+
+    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement> ) => {
+        const { name, value } = e.target;
+        setFormData((prevData) => ({
+          ...prevData,
+          [name]: value,
+        }));
+    };
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        //onSubmit(formData);
+        console.log(formData);
+    };
+
+    return(
+        <>
+            <div className='bg-mine mx-96 rounded-md font-chiliz my-8 text-center min-w-fit'>
+                <div className='p-7'>
+                    <h1 className='text-chilizColor text-3xl font-bold'>Publish your news here !</h1>
+                </div>
+                
+                <form onSubmit={handleSubmit}>
+                    <div className='text-xl'>
+                        <label htmlFor="Title">Title:</label>
+                        <input className='border border-black rounded-md p-2' type="text" id="Title" name="Title" value={formData.Title} onChange={handleInputChange} required />
+                    </div>
+
+                    <div className='text-xl my-3'>
+                        <label htmlFor="Image">Image URL:</label>
+                        <input className='border border-black rounded-md p-2' type="text" id="Image" name="Image" value={formData.Image} onChange={handleInputChange} required />
+                    </div>
+
+
+                    <div className='text-xl flex items-center justify-center'>
+                        <label htmlFor="Content">Content:</label>
+                        <textarea className='w-96 border border-black rounded-md p-2' id="Content" name="Content" value={formData.Content} onChange={handleInputChange} required />
+                    </div>
+          
+                    <button className='text-xl my-3 hover:text-chilizColor' type="submit">Submit</button>
+                </form>
+            </div>
+        </>
+    )
+}
+
+export default publish;
